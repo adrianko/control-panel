eafde0a903cc7fe9cce3cccd4a3fb5af7913c2c3
AK <adrianchko@gmail.com>
2015-03-03 21:05:43 +0000

    Basic http response capabilities

--- src/main/ControlPanel.java
+++ src/main/ControlPanel.java
@@ -1,9 +1,45 @@
 package main;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class ControlPanel {
 
     public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(9999), 0);
+            server.createContext("/", new Home());
+            server.setExecutor(null);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void send(HttpExchange t, String response) {
+        try {
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class Home implements HttpHandler {
+
+        public void handle(HttpExchange t) {
+            String response = "<h1>Hello World</h1>";
+            send(t, response);
+        }
 
     }
+
 
 }
