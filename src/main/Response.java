b974854e0ad2b19e7707d9be05a9be9c74bbb7a1
Adrian Ko <adrianchko@gmail.com>
2015-03-04 12:51:09 +0000

    Create Response class

--- src/main/Response.java
+++ src/main/Response.java
@@ -1,4 +1,22 @@
 package main;
 
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Response {
+    
+    public static void send(HttpExchange t, String response, int responseCode, String contentType) {
+        try {
+            t.getResponseHeaders().add("Content-Type", contentType);
+            t.sendResponseHeaders(responseCode, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
