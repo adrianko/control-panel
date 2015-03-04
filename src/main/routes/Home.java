254b309caa681e02c45436b98395448c3daba812
Adrian Ko <adrianchko@gmail.com>
2015-03-04 13:20:44 +0000

    Handle 404

--- src/main/routes/Home.java
+++ src/main/routes/Home.java
@@ -1,6 +1,10 @@
 package main.routes;
 
+import com.sun.net.httpserver.HttpExchange;
+import main.Response;
 import main.scaffolding.HttpResponse;
+
+import java.io.IOException;
 
 public class Home extends HttpResponse {
     
@@ -17,6 +21,15 @@ public class Home extends HttpResponse {
     @Override
     public int getCode() {
         return 200;
+    }
+
+    public void handle(HttpExchange t) throws IOException {
+        if (!t.getRequestURI().getPath().equals("/")) {
+            HttpResponse err = new Error404();
+            Response.send(t, err.getResponse(), err.getCode(), err.getContentType());
+        } else {
+            Response.send(t, getResponse(), getCode(), getContentType());
+        }
     }
     
 }
