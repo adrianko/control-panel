bf4e868cdfa23097fa6ed96e867217b34040dbf7
Adrian Ko <adrianchko@gmail.com>
2015-03-04 12:51:02 +0000

    Create variable routes

--- src/main/ControlPanel.java
+++ src/main/ControlPanel.java
@@ -1,11 +1,8 @@
 package main;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class ControlPanel {
@@ -13,34 +10,12 @@ public class ControlPanel {
     public static void main(String[] args) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(9999), 0);
-            server.createContext("/", new Home());
+            Routes.urls.forEach(server::createContext);
             server.setExecutor(null);
             server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public static void send(HttpExchange t, String response, int responseCode, String contentType) {
-        try {
-            t.getResponseHeaders().add("Content-Type", contentType);
-            t.sendResponseHeaders(responseCode, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static class Home implements HttpHandler {
-
-        public void handle(HttpExchange t) {
-            String response = "<h1>Hello World</h1>";
-            send(t, response, 200, "text/html");
-        }
-
-    }
-
-
+    
 }
