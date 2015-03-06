71315adc35739a6f48d7510aacc98ade4edd3a43
Adrian Ko <adrianchko@gmail.com>
2015-03-06 15:34:52 +0000

    move into controllers package

--- src/main/Routes.java
+++ src/main/Routes.java
@@ -5,7 +5,7 @@ import com.sun.net.httpserver.HttpHandler;
 import java.util.HashMap;
 import java.util.Map;
 
-import main.routes.*;
+import main.controllers.*;
 
 public class Routes {
     
--- src/main/controllers/Home.java
+++ src/main/controllers/Home.java
@@ -1,4 +1,4 @@
-package main.routes;
+package main.controllers;
 
 import main.scaffolding.HttpResponse;
 
