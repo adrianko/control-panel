9e0bc3c2a434c155ec85ffc6be99548343f0942c
Adrian Ko <adrianchko@gmail.com>
2015-03-06 10:55:34 +0000

    add helper methods

--- src/main/Helper.java
+++ src/main/Helper.java
@@ -1,4 +1,20 @@
 package main;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Helper {
+
+    public static String currentTime() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis()));
+    }
+
+    public static String stamp(String s) {
+        return currentTime() + ": " + s;
+    }
+
+    public static void log(String s) {
+        System.out.println(stamp(s));
+    }
+    
 }
