6c828b2135220d8a37c35d165b750be31b14a52e
Adrian Ko <adrianchko@gmail.com>
2015-03-06 17:32:04 +0000

    fix request

--- src/main/scaffolding/Handler.java
+++ src/main/scaffolding/Handler.java
@@ -17,7 +17,7 @@ public class Handler implements HttpHandler {
     public void handle(HttpExchange t) {
         log("Request: " + t.getRequestURI().getPath());
         Response.send(t, (HttpResponse) routes.getRoutes().get(
-            !routes.getRoutes().containsKey(t.getRequestURI().getPath()) ? t.getRequestURI().getPath() : "/404"
+            routes.getRoutes().containsKey(t.getRequestURI().getPath()) ? t.getRequestURI().getPath() : "/404"
         ));
     }
     
