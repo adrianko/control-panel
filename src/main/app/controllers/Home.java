6cb127f0a58c4bea3e7aa13fafb75c91ddab0d99
AK <adrianchko@gmail.com>
2015-03-08 10:26:46 +0000

    read query parameters in controller

--- src/main/app/controllers/Home.java
+++ src/main/app/controllers/Home.java
@@ -6,11 +6,10 @@ public class Home extends HttpResponse {
 
     @Override
     public String getResponse() {
-        /*
         if (httpRequest.getRequestURI().getQuery().contains("g")) {
             return "<h1>Hi there</h1>";
         }
-        */
+        
         return "<h1>Hello World</h1>";
     }
 
