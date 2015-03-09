5ad26618f4ccb124f80a144235a92ea262409c7b
AK <adrianchko@gmail.com>
2015-03-09 22:01:56 +0000

    add sleep command

--- src/main/app/controllers/Sleep.java
+++ src/main/app/controllers/Sleep.java
@@ -1,4 +1,33 @@
 package main.app.controllers;
 
-public class Sleep {
+import main.scaffolding.HttpResponse;
+
+import java.io.IOException;
+
+public class Sleep extends HttpResponse {
+
+    @Override
+    public void parseRequest() {
+        try {
+            Runtime.getRuntime().exec("C:/sleep.bat");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getResponse() {
+        return "";
+    }
+
+    @Override
+    public String getContentType() {
+        return "text/html";
+    }
+
+    @Override
+    public int getCode() {
+        return 200;
+    }
+
 }
