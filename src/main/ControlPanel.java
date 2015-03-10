cc1c9ea81c4140a119d07eb0ecf2f5be440c8554
Adrian Ko <adrianchko@gmail.com>
2015-03-10 15:38:08 +0000

    add todo

--- src/main/ControlPanel.java
+++ src/main/ControlPanel.java
@@ -2,8 +2,13 @@ package main;
 
 import main.core.Server;
 
+/**
+ * TODO Add default controllers for error pages
+ * TODO Add model classes
+ * TODO Add mustache for views
+ */
 public class ControlPanel {
-
+    
     public static void main(String[] args) {
         new Server(9999);
     }
