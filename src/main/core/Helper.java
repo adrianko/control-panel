19a5e78e95f16263dffe591c631989db9e6fd4da
Adrian Ko <adrianchko@gmail.com>
2015-03-16 09:57:07 +0000

    remove unused methods

--- src/main/core/Helper.java
+++ src/main/core/Helper.java
@@ -1,20 +1,10 @@
 package main.core;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Logger;
 
 public class Helper {
     
     public static Logger logger = Logger.getLogger(Helper.class.getName());
-
-    public static String currentTime() {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(System.currentTimeMillis()));
-    }
-
-    public static String stamp(String s) {
-        return currentTime() + ": " + s;
-    }
 
     public static void log(String s) {
         logger.info(s);
