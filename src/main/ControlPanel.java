3132aea6af07fe0da656569872e12cb1bb9abeec
AK <adrianchko@gmail.com>
2015-04-25 15:57:29 +0100

    Add exit actionlistener

--- src/main/ControlPanel.java
+++ src/main/ControlPanel.java
@@ -4,16 +4,13 @@ import main.core.Base;
 import main.core.Server;
 
 import java.awt.AWTException;
-import java.awt.Image;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.Toolkit;
 import java.awt.TrayIcon;
-import java.awt.event.ActionListener;
 
 /**
- * TODO Add model classes
  * TODO Add mustache for views
  */
 public class ControlPanel {
@@ -22,19 +19,24 @@ public class ControlPanel {
         setTrayIcon(Base.path + "/assets/img/icon.png");
     }
 
+    private static void startServer() {
+        new Server("0.0.0.0", 9999);
+    }
+
     private static void setTrayIcon(String imagePath) {
         if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-            Image image = Toolkit.getDefaultToolkit().getImage(imagePath);
-            ActionListener al = e -> new Server("0.0.0.0", 9999);
-            PopupMenu popup = new PopupMenu();
             MenuItem defaultItem = new MenuItem();
-            defaultItem.addActionListener(al);
+            defaultItem.setLabel("Exit");
+            defaultItem.addActionListener(e -> System.exit(0));
+
+            PopupMenu popup = new PopupMenu();
             popup.add(defaultItem);
-            TrayIcon trayIcon = new TrayIcon(image, "Control Panel", popup);
-            trayIcon.addActionListener(al);
+
+            TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage(imagePath), "Control Panel", popup);
+            trayIcon.addActionListener(e -> startServer());
 
             try {
+                SystemTray tray = SystemTray.getSystemTray();
                 tray.add(trayIcon);
             } catch (AWTException e) {
                 e.printStackTrace();
