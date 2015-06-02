f2d2100269d9863d50b1d0c8450a23f6381d13b4
Adrian Ko <adrianchko@gmail.com>
2015-06-02 08:17:09 +0100

    Add better base path generation

--- src/main/core/Base.java
+++ src/main/core/Base.java
@@ -2,6 +2,10 @@ package main.core;
 
 public class Base {
     
-    public static String path = Base.class.getResource(".").getPath() + "../../../../..";
+    public static String os = System.getProperty("os.name");
+
+    public static String path = (os.contains("Windows") && Base.class.getResource(".").getPath().startsWith("/") ?
+            Base.class.getResource(".").getPath().substring(1) : Base.class.getResource(".").getPath())
+            + "../../../../..";
     
 }
